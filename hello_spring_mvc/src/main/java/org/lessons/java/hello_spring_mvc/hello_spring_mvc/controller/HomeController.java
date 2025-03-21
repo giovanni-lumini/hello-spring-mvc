95f5393be89a8ace912f1e8ffa75cf06bc991e53
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-03-21 14:40:25 +0100

    request-params-myProfile page

--- hello_spring_mvc/src/main/java/org/lessons/java/hello_spring_mvc/hello_spring_mvc/controller/HomeController.java
+++ hello_spring_mvc/src/main/java/org/lessons/java/hello_spring_mvc/hello_spring_mvc/controller/HomeController.java
@@ -1,8 +1,10 @@
 package org.lessons.java.hello_spring_mvc.hello_spring_mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -18,7 +20,9 @@ public class HomeController {
     }
 
     @GetMapping("/my-profile")
-    public String myProfile() {
+    public String myProfile(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("email", "giovanni@gmail.com");
         return "myProfile";
     }
 }
